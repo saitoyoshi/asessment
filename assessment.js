44b4a57abe0c19f852e9e59e8f98db7ae368dd22
saitoyoshi <89641313+saitoyoshi@users.noreply.github.com>
2023-03-12 17:36:03 +0900

    bootstrapを適応

--- assessment.js
+++ assessment.js
@@ -10,14 +10,24 @@ assessmentButton.onclick = () => {
     }
 
     resultDivided.innerText = '';
-    const header = document.createElement('h3');
-    header.innerText = '診断結果';
-    resultDivided.appendChild(header);
+    const headerDivided = document.createElement('div');
+    headerDivided.setAttribute('class', 'card-header');
+    headerDivided.innerText = '診断結果';
+
+    const bodyDivided = document.createElement('div');
+    bodyDivided.setAttribute('class', 'card-body');
 
     const paragraph = document.createElement('p');
+    paragraph.setAttribute('class', 'card-text');
     const result = assessment(userName);
     paragraph.innerText = result;
-    resultDivided.appendChild(paragraph);
+    bodyDivided.appendChild(paragraph);
+
+    resultDivided.setAttribute('class', 'card');
+    resultDivided.setAttribute('style', 'max-width: 700px;')
+
+    resultDivided.appendChild(headerDivided);
+    resultDivided.appendChild(bodyDivided);
 
     tweetDivided.innerText = '';
     const anchor = document.createElement('a');
